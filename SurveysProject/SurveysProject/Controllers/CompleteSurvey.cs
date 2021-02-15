83f818cb67e3a72c25597a4f9cda811d0cc45f1a
Roxana Olteanu <roxana.olteanu@siemens.com>
2021-02-15 11:22:56 +0200

    Display questions one at a time on response page

--- SurveysProject/SurveysProject/Controllers/CompleteSurvey.cs
+++ SurveysProject/SurveysProject/Controllers/CompleteSurvey.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using SurveysProject.Models;
 using SurveysProject.Services.Interfaces;
 using SurveysProject.Models.Data;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SurveysProject.Models.ViewModels;
 
 namespace SurveysProject.Controllers
 {
@@ -23,16 +26,52 @@ namespace SurveysProject.Controllers
             this.questionService = questionService;
             this.responseService = responseService;
         }
-        public IActionResult Index()
+
+        public IActionResult Index(int surveyId)
         {
-            return View();
+            Survey survey = surveyService.GetSurvey(surveyId);
+
+            // verifica daca survey-ul are intrebari (si optiuni), daca nu, vei avea eroare
+
+
+            List<Question> questions = questionService.GetQuestionsForSurvey(surveyId).Take(1).ToList();
+            survey.Questions = new List<Question>();
+            foreach (Question question in questions)
+            {
+                List<QuestionOption> options = questionService.GetOptionsForQuestion(question.QuestionId);
+                question.Options = new List<QuestionOption>();
+                question.Options.AddRange(options);
+                survey.Questions.Add(question);
+            }
+            ResponseQuestion respQ = new ResponseQuestion()
+            {
+                Survey = survey,
+                Question = questions[0],
+            };
+
+            ViewBag.page = 0;
+            return View("Views/CompleteSurvey/Index.cshtml", respQ);
         }
 
-        public ActionResult GetSurvey(int surveyId)
+        public ActionResult AddResponse(int surveyId, int page, ResponseQuestion response)
         {
-            DataModel model = new DataModel();
             Survey survey = surveyService.GetSurvey(surveyId);
-            List<Question> questions = questionService.GetQuestionsForSurvey(surveyId);
+            // get option by id (response.SelectedoptionId) si tot ce iti trebuie
+            // creeaza response si adauga in bd
+
+            page++;
+            ViewBag.page = page;
+
+            List<Question> questions = questionService.GetQuestionsForSurvey(surveyId).Skip(page).Take(1).ToList();
+
+            // daca nu mai sunt alte intrebari
+            if (questions.Count <= 0)
+            {
+                List<Survey> surveys = surveyService.GetSurveys();
+                return View("Views/Home/Index.cshtml", surveys);
+                // sau poti crea o pagina cu o imagine cu bifa si un mesaj cu The survey was successfully completed!
+            }
+
             survey.Questions = new List<Question>();
             foreach (Question question in questions)
             {
@@ -42,23 +81,13 @@ namespace SurveysProject.Controllers
                 survey.Questions.Add(question);
             }
 
-            model.Survey = survey;
-            return View("Views/CompleteSurvey/Index.cshtml", model);
+            ResponseQuestion respQ = new ResponseQuestion()
+            {
+                Survey = survey,
+                Question = questions[0],
+            };
 
-            
-        }
-
-        public ActionResult AddResponse(Response response)
-        {
-            Response response1 = new Response();
-            DataModel model = new DataModel();
-            response1.Question = response.Question;
-            response1.QuestionOption = response.QuestionOption;
-            response1.Survey = response.Survey;
-            responseService.AddResponse(response1);
-
-            model.Survey = response.Survey;
-            return View("Views/CompleteSurvey/Index.cshtml", model);
+            return View("Views/CompleteSurvey/Index.cshtml", respQ);
         }
     }
 }
---
+++ SurveysProject/SurveysProject/Models/ViewModels/ResponseQuestion.cs
@@ -0,0 +1,16 @@
+﻿using SurveysProject.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SurveysProject.Models.ViewModels
+{
+    public class ResponseQuestion
+    {
+        public Survey Survey { get; set; }
+        public Question Question { get; set; }
+
+        public int SelectedOptionId { get; set; }
+    }
+}
